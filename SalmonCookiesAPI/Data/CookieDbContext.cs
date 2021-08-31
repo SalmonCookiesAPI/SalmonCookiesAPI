6805ee925eb62160c74b9c399a2760356c5fd6f5
Joshua Haddock <joshhaddock88@gmail.com>
2021-08-30 18:01:22 -0700

    Got rid of nested folders so Azure can find solution file.

---
+++ SalmonCookiesAPI/Data/CookieDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalmonCookiesAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalmonCookiesAPI.Data
+{
+    public class CookieDbContext : DbContext
+    {
+        public DbSet<Store> Stores { get; set; }
+        
+        public CookieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Store>().HasData(
+                new Store
+                {
+                    Id = 1,
+                    Location = "Seattle",
+                    Description = "",
+                    MinimumCustomers = 4,
+                    MaximumCustomers = 14,
+                    AverageCookiePerSale = 2.5,
+                    Owner = null
+                });
+        }
+    }
+}
---
+++ SalmonCookiesAPI/Models/Interfaces/IStore.cs
@@ -0,0 +1,27 @@
+﻿using SalmonCookiesAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalmonCookiesAPI.Models.Interfaces
+{
+    public interface IStore
+    {
+        // Create
+        Task<Store> Create(Store store);
+
+        // GET ALL
+
+        Task<List<StoreDTO>> GetStores();
+
+        // GET ONE BY ID
+        Task<StoreDTO> GetStore(int id);
+
+        // UPDATE
+        Task<Store> UpdateStore(Store store);
+
+        // DELETE
+        Task Delete(int id);
+    }
+}
